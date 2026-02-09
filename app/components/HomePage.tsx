e3320cba3cb0fc31ad894cdd6d644ab571f8c574
mnkhdlai3600-lgtm <236936562+mnkhdlai3600-lgtm@users.noreply.github.com>
2026-02-09 19:45:22 +0800

    .env

--- app/components/HomePage.tsx
+++ app/components/HomePage.tsx
@@ -1,4 +1,14 @@
+"use client";
+
+import { fetcher } from "@/utils/fetcher";
+import useSWR from "swr";
+
 function HomePage() {
+  const { data, isLoading, error } = useSWR(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/foods/get-food`,
+    fetcher,
+  );
+  console.log(data);
   return (
     <div className="w-screen">
       <img src="offer.png" alt="" />
--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,13 +1,12 @@
-export const fetcherInput = async (endPoint: string) => {
+export const fetcher = async (endPoint: string) => {
   const response = await fetch(endPoint, {
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_TDMB_KEY}`,
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_BACKEND_URL}`,
     },
     cache: "force-cache",
   });
-  console.log(endPoint);
-
   const data = await response.json();
+  console.log(data);
   return data;
 };
